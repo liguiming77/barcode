ab71bf20e06ebeb44279d465d194e28cede92cd3
liguiming77 <1213294227@qq.com>
2022-08-28 10:07:05 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -4,4 +4,7 @@ import barcode
 
 
 code_list = barcode.PROVIDED_BARCODES
-print(code_list)
+# print(code_list)
+code39 = barcode.get_barcode_class('code39')
+code_pic = code39(u'123', writer=ImageWriter())
+fullname = code_pic.save('ean13_barcode')
