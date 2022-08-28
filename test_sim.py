80738830c74025a6f83bb5b5b8b8941a71daa8fb
liguiming77 <1213294227@qq.com>
2022-08-28 09:52:28 +0800

    s

--- test_sim.py
+++ test_sim.py
@@ -1,5 +1,5 @@
 import cv2
 
 bardet = cv2.barcode_BarcodeDetector()
-img = cv2.imread("your file path")
+img = cv2.imread("6.png")
 ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(img)
