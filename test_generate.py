276e61c45a070da9204f2064dcda3130943e64f2
liguiming77 <1213294227@qq.com>
2022-08-29 15:42:04 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -65,7 +65,8 @@ def write_char(ch,outname):
     code_pic = code39(ch, writer=writer)
     fullname = code_pic.save(outname,options={'format':'PNG'})
 
-# write_char('1') #funame=12345.png
+write_char('*') #funame=12345.png
+assert 1>2
 ## exclude * X*
 
 def split_png(fname,idx = 0,exclude=[0,-1,-2]):
