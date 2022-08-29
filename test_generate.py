1509a25d9f6bb3bc27a687d8f228a832589182f4
liguiming77 <1213294227@qq.com>
2022-08-29 13:48:28 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -2,6 +2,8 @@ from barcode.writer import ImageWriter
 from barcode.ean import EuropeanArticleNumber13
 import barcode
 from PIL import Image
+import numpy as np
+
 ## https://www.cnpython.com/pypi/pybarcode
 code_list = barcode.PROVIDED_BARCODES
 # print(code_list) SVG
@@ -13,6 +15,19 @@ writer = ImageWriter()
 code39 = barcode.get_barcode_class('code39')
 # code_pic = code39(u'123', writer=writer)
 # fullname = code_pic.save('code39_barcode')
+def diff_list(ls):
+
+    ls_fore = ls[1:]
+    ls_fore.insert(ls[0])
+    ls = np.asarray(ls)
+    ls_fore = np.asarray(ls_fore)
+    hold_place = np.zeros(ls.size)
+    diff = ls - ls_fore
+    index_no_equal_0 = np.where(diff!=0)
+    hold_place[index_no_equal_0] = 1
+    assert hold_place.sum()%2==0
+    print(hold_place.sum()//2)
+
 def write_char(ch=None):
     code_pic = code39(ch, writer=writer)
     fullname = code_pic.save(ch,options={'format':'PNG'})
@@ -20,5 +35,11 @@ def write_char(ch=None):
 write_char('123456789') #funame=12345.png
 def split_png(fname):
     img = Image.open(fname).convert('L')
-    print(img.size)
+    img_np = np.asarray(img)
+    h,w = img_np.shape
+    h_s = h//2
+    w_points = img_np[h_s]
+    diff_list(w_points)
+
+
 split_png('123456789.png')
