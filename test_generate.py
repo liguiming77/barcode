8762e05acb1d9be408db622bc54fc70a11388306
liguiming77 <1213294227@qq.com>
2022-08-29 18:46:55 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -115,7 +115,7 @@ for idx,char in id2chars.items():
     #print(pic1.shape)
     #print(pic2.shape)
     # pic2 = np.transpose(pic2)
-    #print(pic2.shape)
+    print(pic2.shape)
     # pic2 = np.resize(pic2,dest_size)
 
     print(get_cos_similar_multi(pic2,pic1))
