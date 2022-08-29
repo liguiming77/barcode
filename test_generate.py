5042ddb18709bb16d8799ef3d6f71988480f893a
liguiming77 <1213294227@qq.com>
2022-08-29 14:51:16 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -3,6 +3,7 @@ from barcode.ean import EuropeanArticleNumber13
 import barcode
 from PIL import Image
 import numpy as np
+import os
 
 ## https://www.cnpython.com/pypi/pybarcode
 code_list = barcode.PROVIDED_BARCODES
@@ -27,22 +28,49 @@ def diff_list(ls):
     diff = ls - ls_fore
     index_no_equal_0 = np.where(diff!=0)
     hold_place[index_no_equal_0] = 1
-    print(hold_place.sum())
+    # print(hold_place.sum())
     assert hold_place.sum()%2==0
-    print(hold_place.sum()//2)
+    # print(hold_place.sum()//2)
+    return hold_place
+def get_width_b2b(ls):
+    idx_equal_1 = np.where(ls==1)
+    idx_fore = idx_equal_1[0:-1]
+    idx = idx_equal_1[1:]
+    idx_fore = np.asarray(idx_fore)
+    idx = np.asarray(idx)
+    diff = idx - idx_fore
+    diff = np.insert(diff,-1,10)
+    return diff,idx_equal_1 ## 每个条纹对应的右边宽度，条纹所在的像素位置
+
+
+
+def get_x1_x2(ls,idx): ## exclude 0,-1,-2
+    dst_np = diff_list(ls)
+    no_slide1 = (idx+1)*10 - 1
+    no_slide2 = no_slide1 + 10
+    each_wds,point_black = get_width_b2b(ls)
+    x1 = point_black[no_slide1]+each_wds[no_slide1]//2
+    x2 = point_black[no_slide2] + each_wds[no_slide2] // 2
+    return x1,x2
+    ### 0,1,3
 
 def write_char(ch=None):
     code_pic = code39(ch, writer=writer)
     fullname = code_pic.save(ch,options={'format':'PNG'})
 
-write_char('123456789') #funame=12345.png
-def split_png(fname):
+# write_char('123456789') #funame=12345.png
+## exclude * X*
+def split_png(fname,idx = 0,exclude=[0,-1,-2]):
     img = Image.open(fname).convert('L')
     img_np = np.asarray(img)
     h,w = img_np.shape
     h_s = h//2
     w_points = img_np[h_s]
-    diff_list(w_points)
+    x1,x2 = get_x1_x2(w_points,idx=0)
+    img_np = img_np[:,x1:x2]
+    img_pic = Image.fromarray(np.uint8(img_np)) # np 2 PIL
+    img_pic.save(os.path.basename(fname)+'_split_pil.png',quality=95)
+    # ls = diff_list(w_points)
 
 
 split_png('123456789.png')
