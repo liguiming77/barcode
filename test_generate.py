791c6ee34cb4fe86f4432e5b5ab81403be8c10e3
liguiming77 <1213294227@qq.com>
2022-08-29 13:13:22 +0800

    s

--- test_generate.py
+++ test_generate.py
@@ -4,7 +4,16 @@ import barcode
 
 ## https://www.cnpython.com/pypi/pybarcode
 code_list = barcode.PROVIDED_BARCODES
-# print(code_list)
+# print(code_list) SVG
+# writer = ImageWriter(options={'format': 'JPEG'})
+# writer = ImageWriter(options={'format': 'SVG'})
+chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%*'
+char_list = [w for w in chars]
+writer = ImageWriter(options={'format': 'PNG'})
 code39 = barcode.get_barcode_class('code39')
-code_pic = code39(u'123', writer=ImageWriter())
-fullname = code_pic.save('code39_barcode')
+# code_pic = code39(u'123', writer=writer)
+# fullname = code_pic.save('code39_barcode')
+def write_char(ch=None):
+    code_pic = code39(ch, writer=writer)
+    fullname = code_pic.save(ch)
+
